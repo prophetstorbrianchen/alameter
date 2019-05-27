49ac0cc6299c60c9839750cfdfdb863c5a6acdbf
brian.chen <brian.chen@prophetstor.com>
2019-05-27 18:14:01 +0800

    [For Alameda v0.3.10] 1.Update alameda version

--- helm/alameter_install.sh
+++ helm/alameter_install.sh
@@ -59,13 +59,13 @@ else
 fi
 
 echo "========Install Alameda========"
-$default_path install stable/influxdb --version 1.1.0 --name influxdb --namespace monitoring
+$default_path install stable/influxdb --version 1.1.3 --name influxdb --namespace alameda
 
-$default_path install stable/prometheus-operator --version 4.3.1 --name prometheus --namespace monitoring -f $alameter_pwd/prometheus-operator/values.yaml
+$default_path install stable/prometheus-operator --version 5.5.1 --name prometheus --namespace monitoring
 
-$default_path install --name alameda --namespace alameda $alameter_pwd/alameda --set image.repository=containersai/alameda-operator-rhel --set image.tag=masterdev.5 --set image.pullPolicy=Always --set evictioner.image.repository=containersai/alameda-evictioner-rhel --set evictioner.image.tag=0.3.6 --set image.pullPolicy=Always --set admission-controller.image.repository=containersai/alameda-admission-rhel --set admission-controller.image.tag=0.3.6 --set image.pullPolicy=Always --set datahub.image.repository=containersai/alameda-datahub-rhel --set datahub.image.tag=masterdev.5 --set datahub.image.pullPolicy=Always --set alameda-ai.image.repository=containersai/alameda-ai --set alameda-ai.image.tag=ri.7 --set alameda-ai.image.pullPolicy=Always --set alameda-ai.image.accessToken=ewogICAgImF1dGhzIjogewogICAgICAgICJodHRwczovL2luZGV4LmRvY2tlci5pby92MS8iOiB7CiAgICAgICAgICAgICJhdXRoIjogIlkyOXVkR0ZwYm1WeWMyRnBPbVZ1YldsMGVTMXVaV1F0YTJ0ciIKICAgICAgICB9CiAgICB9Cn0K
+$default_path install --name alameda --namespace alameda $alameter_pwd/alameda --set image.repository=quay.io/prophetstor/alameda-operator-rhel --set image.tag=v0.3.10 --set image.pullPolicy=Always --set evictioner.image.repository=quay.io/prophetstor/alameda-evictioner-rhel --set evictioner.image.tag=v0.3.10 --set image.pullPolicy=Always --set admission-controller.image.repository=quay.io/prophetstor/alameda-admission-rhel --set admission-controller.image.tag=v0.3.10 --set image.pullPolicy=Always --set datahub.image.repository=quay.io/prophetstor/alameda-datahub-rhel --set datahub.image.tag=v0.3.10 --set datahub.image.pullPolicy=Always --set alameda-ai.image.repository=quay.io/prophetstor/alameda-ai --set alameda-ai.image.tag=v0.3.10 --set alameda-ai.image.pullPolicy=Always
 
-$default_path install --name grafana --namespace monitoring $alameter_pwd/grafana
+$default_path install --name grafana --namespace alameda $alameter_pwd/grafana
 
 echo "========For old datahub name========"
 sleep 5
